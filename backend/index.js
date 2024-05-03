7af2f99a0de9d9f4b4aa122b8e94ca79a97f49c6
PorkyJames <kwanjko17@gmail.com>
2024-05-03 03:30:40 -0700

    Added bcrypt and hashing of user passwords along with the User Schema in the Model folder

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,46 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+// const cors = require("cors");
+import cors from "cors"
+import dotenv from "dotenv";
+
 
 const app = express();
+// require("dotenv").config()
+dotenv.config();
+
+mongoose
+.connect(mongoDBURL, {
+    //! MongoDB uses an older version of URL Parser / topology engine. Setting these to true helps implement new engines to the DB
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("App connected to MongoDB");
+    app.listen(PORT, () => {
+        console.log(`App listening to port: ${ PORT }`)
+    })
+})
+.catch((error) => {
+    console.log(error)
+})
+
+app.use(
+    cors({
+        origin: ["http://localhost:5555"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
+
+app.use(express.json());
+
 
 //! Routes
 app.get("/", (request, response) => {
     console.log(request);
     return response.status(234).send("Welcome to Climbr")
-} )
-
-mongoose
-    .connect(mongoDBURL)
-    .then(() => {
-        console.log("App connected to MongoDB");
-        app.listen(PORT, () => {
-            console.log(`App listening to port: ${ PORT }`)
-        })
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+})
 
 
---
+++ backend/models/UserModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Your email address is required"],
+        unique: true,
+    },
+    username: {
+        type: String,
+        required: [true, "Your username is required"],
+    },
+    password: {
+        type: String,
+        required: [true, "Your password is required"],
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+});
+
+//! This middleware function that every time a user doc is saved to the db, the user's password is hashed using bcrypt 
+//! with a salt and a cost factor of 12. Cost factor basically means increases computational work. However,
+//! the higher we go, the more performance it takes. 12 is a very commonly used cost factor. 
+userSchema.pre("save", async function () {
+    this.password = await bcrypt.hash(this.password, 12);
+});
+
+module.exports = mongoose.model("User", userSchema);
