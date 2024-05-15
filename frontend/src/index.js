71d180317bf8a45b309103225c3e78e0fe992cd7
PorkyJames <kwanjko17@gmail.com>
2024-05-15 16:07:55 -0700

    Started Home, Login, SignUp start pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 
 import { BrowserRouter } from "react-router-dom";
+
+//! For Cool CSS related Notification pop ups
 import 'react-toastify/dist/ReactToastify.css'
 
 //! React V.18 and above
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
