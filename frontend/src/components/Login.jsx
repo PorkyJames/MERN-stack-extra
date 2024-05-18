f7684664b6bcd32479f9edc3de717d9546786f8d
PorkyJames <kwanjko17@gmail.com>
2024-05-17 21:28:25 -0700

    Started Login Page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 
 const Login = () => {
+
+
+    const handleDemoLogin = () => {
+        const demoCredentials = {
+            email: "demouser@gmail.com",
+            password: "DemoLogin"
+        }
+
+        
+
+    }
+
     return (
-        <h1> Login Page </h1>
+        <>
+            <div className="title-button">
+                <button onClick={titleButton}>
+                    <h1> Climbr </h1>
+                </button>
+            </div>
+        </>
     )
 };
 
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
 const SignUp = () => {
+
+    
+
     return (
-        <h1> SignUp Page </h1>
+        <>
+            <h1> SignUp Page </h1>    
+        </>
     )
 };
 
