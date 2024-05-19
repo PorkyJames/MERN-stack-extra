62b0859047250c88b8666fbe07f35aedb7a2bbfd
PorkyJames <kwanjko17@gmail.com>
2024-05-18 22:07:00 -0700

    Added Login page, but error-ing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+//! Routes has been swapped for Switch in react-router-dom v6
+import { Router, Route, Routes } from "react-router-dom"
+
+//! Import Components
+import Login from './components/Login';
 
 function App() {
+
   return (
-    <h1>Hello world!</h1>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" component={Login} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,25 +1,47 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
 
+//! Import variables
+const navigate = useNavigate(); 
 
     const handleDemoLogin = () => {
         const demoCredentials = {
             email: "demouser@gmail.com",
             password: "DemoLogin"
         }
+    }
 
-        
-
+    //! Return Home Button for Title
+    const returnToHome = () => {
+        navigate('/')
     }
 
     return (
         <>
+            {/* Main Page Button */}
             <div className="title-button">
-                <button onClick={titleButton}>
+                <button onClick={returnToHome}>
                     <h1> Climbr </h1>
                 </button>
             </div>
+
+            {/* Login Card */}
+            <div className="login-card">
+                <div className="login-card-title">
+                    <h1>Login</h1>
+                </div>
+
+                <div className="login-card-inputs">
+                    <h1>Inputs</h1>
+                </div>
+
+                <div className="login-card-demo-login-button">
+                    <button>Demo-Login</button>
+                </div>
+            </div>
+
         </>
     )
 };
