7d3e86c6bdf527d45a5dec22b9b62ffc420c74ba
PorkyJames <kwanjko17@gmail.com>
2024-05-26 00:07:22 -0700

    Created the first session thunks for session.js

--- frontend/redux/session.js
+++ frontend/redux/session.js
@@ -1,1 +1,35 @@
 //! Create the login thunk. 
+
+//! Constants
+
+const SET_USER = "session/SET_USER";
+const REMOVE_USER = "session/REMOVE_USER";
+
+const setUser = (user) => ({
+    type: SET_USER,
+    payload: user,
+});
+
+const removeUser = () => ({
+    type: REMOVE_USER,
+})
+
+const initialState = { user: null };
+
+export const authenticate = () => async (dispatch) => {
+    const res = await fetch("/api/auth/", {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+
+    if (res.ok) {
+        const data = await res.json();
+        if (data.errors) {
+            return errors;
+        }
+
+        dispatch(setUser(data));
+    }
+
+}
